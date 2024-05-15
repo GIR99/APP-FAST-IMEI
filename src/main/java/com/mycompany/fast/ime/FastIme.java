6d9c87ecb2b4abcca3e674129a1943457c23f391
GIR99 <gir_j@hotmail.com>
2024-05-14 23:48:42 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/fast/ime/FastIme.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fast.ime;
+
+/**
+ *
+ * @author GIR_J
+ */
+public class FastIme {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
